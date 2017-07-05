fd0f268cad5e33a336e74349e1f11e215f896779
lvPanda <lavf@cin.ufpe.br>
2017-07-05 14:50:02 -0300

    Initial commit

---
+++ mobile/src/main/java/com/gift/lvpanda/bgworld/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.gift.lvpanda.bgworld
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        this.ludopediaLogin.setOnClickListener {
+            val intent = Intent(this,MainActivity :: class.java)
+            this.startActivity(intent)
+            this.finish()
+        }
+    }
+}

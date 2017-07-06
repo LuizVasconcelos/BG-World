63ff6906e6327b39c0b6740dd0d47dba39e42092
lvPanda <lavf@cin.ufpe.br>
2017-07-06 18:23:58 -0300

    Start of integration of Navigation Drawer interaction with Main Activity

--- mobile/src/main/java/com/gift/lvpanda/bgworld/MainActivity.kt
+++ mobile/src/main/java/com/gift/lvpanda/bgworld/MainActivity.kt
@@ -2,12 +2,29 @@ package com.gift.lvpanda.bgworld
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main_navigation_drawer.*
 
 class MainActivity : AppCompatActivity() {
+
+    val mDrawerToggle by lazy { android.support.v7.app.ActionBarDrawerToggle(this,navigationDrawer, 0,0) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        this.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        this.supportActionBar?.setHomeButtonEnabled(true)
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            android.R.id.home -> {
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }

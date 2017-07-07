746b4f7c5ae8f758a6b6c347040af33e6cbb5d19
lvPanda <lavf@cin.ufpe.br>
2017-07-07 18:16:20 -0300

    Make button clickable and flow from login screen to main screen (menu still broken)

--- mobile/src/main/java/com/gift/lvpanda/bgworld/MainActivity.kt
+++ mobile/src/main/java/com/gift/lvpanda/bgworld/MainActivity.kt
@@ -3,12 +3,13 @@ package com.gift.lvpanda.bgworld
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main_navigation_drawer.*
 
 class MainActivity : AppCompatActivity() {
 
-    val mDrawerToggle by lazy { android.support.v7.app.ActionBarDrawerToggle(this,navigationDrawer, 0,0) }
+    val mDrawerToggle by lazy { android.support.v7.app.ActionBarDrawerToggle(this,navigationDrawer, 0,0)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

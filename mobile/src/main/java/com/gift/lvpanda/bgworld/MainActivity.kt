e6a93446a91ed3902007008598cc9d8ba4d061a1
lvPanda <lavf@cin.ufpe.br>
2017-07-12 18:15:21 -0300

    Several adjustments on FAB + start of animations on FAB (also added 2 more FAB for Stores List and Aunctions List)

--- mobile/src/main/java/com/gift/lvpanda/bgworld/MainActivity.kt
+++ mobile/src/main/java/com/gift/lvpanda/bgworld/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import android.view.View
+import android.view.animation.AnimationUtils
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main_navigation_drawer.*
 
 class MainActivity : AppCompatActivity() {
 
-    val mDrawerToggle by lazy { android.support.v7.app.ActionBarDrawerToggle(this,navigationDrawer, 0,0)}
+    val mDrawerToggle by lazy { android.support.v7.app.ActionBarDrawerToggle(this,navigationDrawer, 0,0) }
+    var isFABOpened = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +20,21 @@ class MainActivity : AppCompatActivity() {
         this.supportActionBar?.setDisplayHomeAsUpEnabled(true)
         this.supportActionBar?.setHomeButtonEnabled(true)
 
+        val mainFABOpenAnimation = AnimationUtils.loadAnimation(this.applicationContext,R.anim.rotate_foward)
+        val mainFABCloseAnimation = AnimationUtils.loadAnimation(this.applicationContext,R.anim.rotate_backward)
+
+        this.mainScreenFAB.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                if(!isFABOpened) {
+                    mainScreenFAB.startAnimation(mainFABOpenAnimation)
+                    isFABOpened = true
+                }else {
+                    mainScreenFAB.startAnimation(mainFABCloseAnimation)
+                    isFABOpened = false
+                }
+            }
+        })
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -25,4 +42,5 @@ class MainActivity : AppCompatActivity() {
 
         return super.onOptionsItemSelected(item)
     }
+
 }

f57573cac2441023607976d49697c037488859ca
lvPanda <lavf@cin.ufpe.br>
2017-07-13 15:31:16 -0300

    Animations FABs completed on Main Screen

--- mobile/src/main/java/com/gift/lvpanda/bgworld/MainActivity.kt
+++ mobile/src/main/java/com/gift/lvpanda/bgworld/MainActivity.kt
@@ -14,8 +14,10 @@ class MainActivity : AppCompatActivity() {
     val mDrawerToggle by lazy { android.support.v7.app.ActionBarDrawerToggle(this,navigationDrawer, 0,0) }
     var isFABOpened = false
 
-    //var translatorFAB1 by lazy {TranslateAnimation(0,0,0,0)}
-    //var translatorFAB2
+    lateinit var openTranslatorFABStore: TranslateAnimation
+    lateinit var openTranslatorFABAunctions: TranslateAnimation
+    lateinit var closeTranslatorFABStore: TranslateAnimation
+    lateinit var closeTranslatorFABAunctions: TranslateAnimation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,15 +29,23 @@ class MainActivity : AppCompatActivity() {
         val mainFABOpenAnimation = AnimationUtils.loadAnimation(this.applicationContext,R.anim.rotate_foward)
         val mainFABCloseAnimation = AnimationUtils.loadAnimation(this.applicationContext,R.anim.rotate_backward)
 
-
+        this.prepareFABsTranslateAnimation()
 
         this.mainScreenFAB.setOnClickListener(object: View.OnClickListener {
             override fun onClick(view: View?) {
                 if(!isFABOpened) {
                     mainScreenFAB.startAnimation(mainFABOpenAnimation)
+                    storesListFAB.startAnimation(mainFABOpenAnimation)
+                    storesListFAB.startAnimation(openTranslatorFABStore)
+                    auctionsListFAB.startAnimation(mainFABOpenAnimation)
+                    auctionsListFAB.startAnimation(openTranslatorFABAunctions)
                     isFABOpened = true
                 }else {
                     mainScreenFAB.startAnimation(mainFABCloseAnimation)
+                    storesListFAB.startAnimation(mainFABOpenAnimation)
+                    storesListFAB.startAnimation(closeTranslatorFABStore)
+                    auctionsListFAB.startAnimation(mainFABOpenAnimation)
+                    auctionsListFAB.startAnimation(closeTranslatorFABAunctions)
                     isFABOpened = false
                 }
             }
@@ -51,9 +61,21 @@ class MainActivity : AppCompatActivity() {
 
     fun prepareFABsTranslateAnimation() {
         var mainFABoriginalPos = IntArray(2)
-        mainScreenFAB.getLocationOnScreen(mainFABoriginalPos)
+        this.mainScreenFAB.getLocationOnScreen(mainFABoriginalPos)
 
+        openTranslatorFABStore = TranslateAnimation(mainFABoriginalPos[0].toFloat(), (mainFABoriginalPos[0].toFloat()-(250*0.258819).toFloat()),mainFABoriginalPos[1].toFloat(), mainFABoriginalPos[1].toFloat()-(250*0.965926).toFloat())
+        openTranslatorFABAunctions = TranslateAnimation(mainFABoriginalPos[0].toFloat(), (mainFABoriginalPos[0].toFloat()-(250*0.965926).toFloat()),mainFABoriginalPos[1].toFloat(), mainFABoriginalPos[1].toFloat()-(250*0.258819).toFloat())
+        openTranslatorFABStore.fillAfter = true
+        openTranslatorFABAunctions.fillAfter = true
+        openTranslatorFABStore.duration = 400
+        openTranslatorFABAunctions.duration = 400
 
+        closeTranslatorFABStore = TranslateAnimation((mainFABoriginalPos[0].toFloat()-(250*0.258819).toFloat()), mainFABoriginalPos[0].toFloat(),mainFABoriginalPos[1].toFloat()-(250*0.965926).toFloat(), mainFABoriginalPos[1].toFloat())
+        closeTranslatorFABAunctions = TranslateAnimation((mainFABoriginalPos[0].toFloat()-(250*0.965926).toFloat()), mainFABoriginalPos[0].toFloat(), mainFABoriginalPos[1].toFloat()-(250*0.258819).toFloat(), mainFABoriginalPos[1].toFloat())
+        closeTranslatorFABStore.fillAfter = true
+        closeTranslatorFABAunctions.fillAfter = true
+        closeTranslatorFABStore.duration = 400
+        closeTranslatorFABAunctions.duration = 400
     }
 
 }
